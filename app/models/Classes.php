e806a2eeab943a10bb98d324f1ca741e1bf69928
Andranik Andy Tonoyan <andranik.tonoyan.606@my.csun.edu>
2014-04-30 15:59:15 -0700

    Renamed table name. Created term proprety, which is Semester-Year format of the term code

--- app/models/Classes.php
+++ app/models/Classes.php
@@ -10,15 +10,26 @@ class Classes extends Eloquent{
 	 *
 	 * @var string
 	 */
-	protected $table = 'classes';
+	protected $table = 'course_class';
+	protected $hidden = array('created_at', 'updated_at');
 
-	public function classmeeting()
+	protected $appends = array('term');
+
+	public function class_meeting()
 	{
 		return $this->hasOne('ClassMeeting', 'class_number', 'class_number');
 	}
 
-	public function classinstructors()
+	public function class_instructors()
 	{
 		return $this->hasMany('ClassInstructor', 'class_number', 'class_number');
+	}
+
+	//Convert semester term code to Semester-Year format
+	public function getTermAttribute()
+	{
+		$term = $this->sterm;
+		$term_codes = array(7 => 'Fall', 3 => 'Spring');
+		return $term_codes[$term[3]] . '-' . $term[0] . '0' . $term[1] . $term[2];
 	}
 }
