1529920a34c046b7ba0425a9257c8291876f809c
Andranik Andy Tonoyan <andranik.tonoyan.606@my.csun.edu>
2014-04-30 15:57:43 -0700

    Hid created_at and updated_at. Renamed table name. Converted start_time and end_time timestamps to military time for the JSON response

--- app/models/ClassMeeting.php
+++ app/models/ClassMeeting.php
@@ -10,5 +10,22 @@ class ClassMeeting extends Eloquent{
 	 *
 	 * @var string
 	 */
-	protected $table = 'classmeetings';
+	protected $table = 'class_meeting';
+	protected $hidden = array('created_at', 'updated_at');
+
+	protected $appends = array('start_time', 'end_time');
+
+	//Change start_time from timestamp to military time
+	public function getStartTimeAttribute($value)
+	{
+		$date = new DateTime($value);
+		return $date->format('Hi') . 'h';
+	}
+
+	//Change end_time from timestamp to military time
+	public function getEndTimeAttribute($value)
+	{
+		$date = new DateTime($value);
+		return $date->format('Hi') . 'h';
+	}
 }
