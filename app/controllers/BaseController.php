9c0f57de161d4b93c6a466c8c368c2b0da0099c6
Ursula Messick <ursula.messick.95@my.csun.edu>
2014-07-15 14:45:50 -0700

    Make use of Clockwork
    Use clockwork in Base Controller so events can be logged.

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -2,6 +2,18 @@
 
 class BaseController extends Controller {
 
+	public function __construct()
+	{
+		/* The following allows the Clockwork Google Chrome extension to work, MUST be in debug mode */
+		$this->beforeFilter(function() {
+		    Event::fire('clockwork.controller.start');
+		});
+
+		$this->afterFilter(function() {
+		    Event::fire('clockwork.controller.end');
+		});
+	}
+
 	/**
 	 * Setup the layout used by the controller.
 	 *
