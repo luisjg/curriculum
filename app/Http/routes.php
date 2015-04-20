0b6b0d342731ebbf9780ca685021198ce9dd359b
Matthew Fritz <matthew.fritz@csun.edu>
2015-04-20 16:39:07 -0700

    Added the public files and copied over the routes file

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,11 +11,24 @@
 |
 */
 
-Route::get('/', 'WelcomeController@index');
+// class info with current term
+Route::get('classes', 'ClassController@index');
+Route::get('classes/{id}', 'ClassController@show');
 
-Route::get('home', 'HomeController@index');
+// class info with specific term
+Route::get('terms/{term}/classes', 'TermController@classesIndex');
+Route::get('terms/{term}/classes/{id}', 'TermController@classesShow');
 
-Route::controllers([
-	'auth' => 'Auth\AuthController',
-	'password' => 'Auth\PasswordController',
-]);
+// course info with current term
+Route::get('courses', 'CourseController@index');
+Route::get('courses/{id}', 'CourseController@show');
+
+// course info with specific term
+Route::get('terms/{term}/courses', 'TermController@coursesIndex');
+Route::get('terms/{term}/courses/{id}', 'TermController@coursesShow');
+
+// landing page and catch-all
+Route::get('/', function()
+{
+	return view('hello');
+});
