4eaebcc0d450422cf072fda342be4bed1f2bef2a
Matthew Fritz <matthew.fritz@csun.edu>
2015-04-22 13:31:26 -0700

    Reworked the landing page for better Blade integrity

--- resources/views/pages/hello.blade.php
+++ resources/views/pages/hello.blade.php
@@ -7,26 +7,26 @@
 		<h2>Examples</h2>
 		<dl>
 			<dt>Course Classes By Subject</dt>
-			<dd><?php echo link_to('api/classes/comp'); ?></dd>
-			<dd><?php echo link_to('api/classes/comp-110'); ?></dd>
-			<dd><?php echo link_to('api/terms/Fall-2014/classes/comp-110'); ?></dd>
+			<dd>{!! link_to('api/classes/comp') !!}</dd>
+			<dd>{!! link_to('api/classes/comp-110') !!}</dd>
+			<dd>{!! link_to('api/terms/Fall-2014/classes/comp-110') !!}</dd>
 
 			<dt>Course Classes Taught by Instructor</dt>
-			<dd><?php echo link_to('api/classes?instructor=harry.hellenbrand@csun.edu'); ?></dd>
+			<dd>{!! link_to('api/classes?instructor=harry.hellenbrand@csun.edu') !!}</dd>
 
 			<dt>Single Class</dt>
-			<dd><?php echo link_to('api/classes/15223'); ?></dd>
+			<dd>{!! link_to('api/classes/15223') !!}</dd>
 
 			<dt>Course Listings</dt>
-			<dd><?php echo link_to('api/courses/comp'); ?></dd>
-			<dd><?php echo link_to('api/terms/Fall-2014/courses/comp'); ?></dd>
+			<dd>{!! link_to('api/courses/comp') !!}</dd>
+			<dd>{!! link_to('api/terms/Fall-2014/courses/comp') !!}</dd>
 
 
 			<dt>Non-Current Term Classes</dt>
-			<dd><?php echo link_to('api/terms/Fall-2013/classes/comp'); ?></dd>
-			<dd><?php echo link_to('api/terms/Spring-2013/classes/comp'); ?></dd>
-			<dd><?php echo link_to('api/terms/Fall-2014/classes/comp-322'); ?></dd>
-			<dd><?php echo link_to('api/terms/Fall-2014/classes?instructor=steven.fitzgerald@csun.edu'); ?></dd>
+			<dd>{!! link_to('api/terms/Fall-2013/classes/comp') !!}</dd>
+			<dd>{!! link_to('api/terms/Spring-2013/classes/comp') !!}</dd>
+			<dd>{!! link_to('api/terms/Fall-2014/classes/comp-322') !!}</dd>
+			<dd>{!! link_to('api/terms/Fall-2014/classes?instructor=steven.fitzgerald@csun.edu') !!}</dd>
 		</dl>
 
 	</div>
