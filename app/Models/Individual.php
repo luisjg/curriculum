96739604ff77a3bde1b9d7cd2594abae78b3654e
Matthew Fritz <matthew.fritz@csun.edu>
2015-04-22 12:48:01 -0700

    Added in the auth models

---
+++ app/Models/Individual.php
@@ -0,0 +1,38 @@
+<?php namespace Curriculum\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Individual extends Model
+{
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'nemo.individuals';
+
+	/**
+	 * Primary key in the table relationship.
+	 *
+	 * @var string
+	 */
+	protected $primaryKey = 'individuals_id';
+
+	/**
+	 * Returns whether this individual is active.
+	 *
+	 * @return boolean
+	 */
+	public function isActive() {
+		return $this->affiliation_status == "Active";
+	}
+
+	/**
+	 * Returns the user associated with this individual.
+	 *
+	 * @return User
+	 */
+	public function user() {
+		return $this->belongsTo("Curriculum\Models\User", "individuals_id");
+	}
+}
---
+++ app/Models/Membership.php
@@ -0,0 +1,33 @@
+<?php namespace Curriculum\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Membership extends Model
+{
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'nemo.memberships';
+
+	/**
+	 * The primary key for the relationship. In reality this is a composite.
+	 *
+	 * @var string
+	 */
+	protected $primaryKey = 'individuals_id';
+
+	/**
+	 * The attributes that can be auto-filled in the model.
+	 *
+	 * @var array
+	 */
+	protected $fillable = [
+		'parent_entities_id',
+		'individuals_id',
+		'role_position',
+		'ad_hoc_member',
+		'confidential'
+	];
+}
