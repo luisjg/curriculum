37b27dd61612d0419ca12ddbd2a95cf8cd12f6e3
arthurwirsching <arthur.wirsching.22@my.csun.edu>
2017-10-10 13:30:27 -0700

    Initial Commit
    Added two test functions for the function classesShow. Total of 12 assertions.

--- tests/TermControllerTest.php
+++ tests/TermControllerTest.php
@@ -2,15 +2,14 @@
 
 use Curriculum\Http\Controllers\ClassController;
 
-class ClassControllerTest extends TestCase
+class TermControllerTest extends TestCase
 {
-    protected $classController;
     protected $classId;
     public function setUp(){
         parent::setUp();
       $this->classId = 'comp-100';
     }
-    public function testShow_shows_class(){
+    public function testClassesShow_shows_classes(){
         $data = $this->call('GET', 'api/1.1/terms/Spring-2015/classes/' . $this->classId);
         $content = json_decode($data->getContent(), true);
         $this->assertEquals($content['api'],'curriculum');
@@ -18,6 +17,16 @@ class ClassControllerTest extends TestCase
         $this->assertEquals($content['success'],'true');
         $this->assertEquals($content['version'],'1.1');
         $this->assertEquals($content['collection'],'classes');
+        $this->assertEquals(count($content['classes']),17);
+    }
+    public function testClassesShow_shows_legacy_classes(){
+        $data = $this->call('GET', 'terms/Spring-2015/classes/' . $this->classId);
+        $content = json_decode($data->getContent(), true);
+        $this->assertEquals($content['api'],'curriculum');
+        $this->assertEquals($content['status'],'200');
+        $this->assertEquals($content['success'],'true');
+        $this->assertEquals($content['version'],'1.0');
+        $this->assertEquals($content['type'],'classes');
         $this->assertEquals(count($content['classes']),17);
     }
 }
