626bd2c54e2599e9cab2641c96b96792eea79d8e
joesdev <joesilvadev@gmail.com>
2017-10-11 15:24:01 -0700

    Added function to test index()

--- tests/PlanControllerTest.php
+++ tests/PlanControllerTest.php
@@ -8,8 +8,14 @@ class PlanControllerTest extends TestCase
         parent::setUp();
     }
 
-    public function testIndex_returns_json_content()
-    {
-
+    public function testIndex_returns_json_content_for_all_versions()
+    {   $data = $this->call('GET','/plans');
+        $content = json_decode($data->getContent(), true);
+        $this->assertEquals($content['api'], 'curriculum');
+        $this->assertEquals($content['status'], '200');
+        $this->assertEquals($content['version'], '1.1');
+        $this->assertEquals($content['collection'], 'plans');
+        $this->assertEquals($content['limit'], '150');
+        $this->assertEquals(count($content['plans']), 148);
     }
 }
