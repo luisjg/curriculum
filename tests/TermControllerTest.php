f434c87598d16e3836c8b8bbf2807a963d5af356
joesdev <joesilvadev@gmail.com>
2017-10-11 12:30:45 -0700

    Created two test functions for coursesIndex(), working

--- tests/TermControllerTest.php
+++ tests/TermControllerTest.php
@@ -32,6 +32,28 @@ class TermControllerTest extends TestCase {
         $this->assertEquals(count($content['classes']), 2);
     }
 
+    public function testCoursesIndex_returns_json_content_for_version_one()
+    {
+        $data = $this->call('GET','/terms/2153/courses');
+        $content = json_decode($data->getContent(), true);
+        $this->assertEquals($content['version'], '1.0');
+        $this->assertEquals($content['api'], 'curriculum');
+        $this->assertEquals($content['status'], 200);
+        $this->assertEquals($content['success'], 'true');
+        $this->assertEquals($content['type'], 'courses');
+        $this->assertEquals(count($content['courses']), 2878);
+    }
 
+    public function testCoursesIndex_returns_json_content_for_version_one_point_one()
+    {
+        $data = $this->call('GET','api/1.1/terms/2153/courses');
+        $content = json_decode($data->getContent(), true);
+        $this->assertEquals($content['version'], '1.1');
+        $this->assertEquals($content['api'], 'curriculum');
+        $this->assertEquals($content['status'], 200);
+        $this->assertEquals($content['success'], 'true');
+        $this->assertEquals($content['collection'], 'courses');
+        $this->assertEquals(count($content['courses']), 2878);
+    }
 }
 
