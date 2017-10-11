530a7b244bebb020c3b603c75e8652a9e139fe78
joesdev <joesilvadev@gmail.com>
2017-10-11 15:58:02 -0700

    removed unnecesary use

--- tests/ClassControllerTest.php
+++ tests/ClassControllerTest.php
@@ -1,7 +1,5 @@
 <?php
 
-use Curriculum\Http\Controllers\ClassController;
-
 class ClassControllerTest extends TestCase
 {
     protected $validEmail;
@@ -20,6 +18,18 @@ class ClassControllerTest extends TestCase
         $this->assertEquals($content['status'],'200');
         $this->assertEquals($content['success'],'true');
         $this->assertEquals($content['type'],'classes');
+        $this->assertEquals(count($content['classes']),3);
+    }
+
+    public function testIndex_returns_json_content_for_version_one_point_one()
+    {
+        $data = $this->call('GET', 'api/1.1/classes?instructor='.$this->validEmail);
+        $content = json_decode($data->getContent(), true);
+        $this->assertEquals($content['api'],'curriculum');
+        $this->assertEquals($content['status'],'200');
+        $this->assertEquals($content['success'],'true');
+        $this->assertEquals($content['version'],'1.1');
+        $this->assertEquals($content['collection'],'classes');
         $this->assertEquals(count($content['classes']),3);
     }
 }
