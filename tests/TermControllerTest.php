afaae8bf50689f63caab3f6f5d6ece44e19d6f1f
joesdev <joesilvadev@gmail.com>
2017-10-11 13:59:28 -0700

    Started test functio for coursesShow

--- tests/TermControllerTest.php
+++ tests/TermControllerTest.php
@@ -3,15 +3,20 @@ use Curriculum\Http\Controllers\TermController;
 
 class TermControllerTest extends TestCase {
 
-    protected $validEmail = 'steven.fitzgerald@csun.edu';
+    protected $validEmail;
+    protected $validTerm;
+    protected $validClassId;
 
     public function setUp(){
         parent::setUp();
+        $this->validEmail = 'steven.fitzgerald@csun.edu';
+        $this->validTerm = 2153;
+        $this->validClassId = 19149;
     }
 
     public function testClassesIndex_returns_json_content_for_version_one()
     {
-        $data = $this->call('GET','/terms/2153/classes?instructor='.$this->validEmail);
+        $data = $this->call('GET','/terms/' . $this->validTerm . '/classes?instructor='.$this->validEmail);
         $content = json_decode($data->getContent(), true);
         $this->assertEquals($content['version'], '1.0');
         $this->assertEquals($content['api'], 'curriculum');
@@ -22,7 +27,7 @@ class TermControllerTest extends TestCase {
 
     public function testClassesIndex_returns_json_content_for_version_one_point_one()
     {
-        $data = $this->call('GET','api/1.1/terms/2153/classes?instructor='.$this->validEmail);
+        $data = $this->call('GET','api/1.1/terms/' . $this->validTerm . '/classes?instructor='.$this->validEmail);
         $content = json_decode($data->getContent(), true);
         $this->assertEquals($content['version'], '1.1');
         $this->assertEquals($content['api'], 'curriculum');
@@ -34,7 +39,7 @@ class TermControllerTest extends TestCase {
 
     public function testCoursesIndex_returns_json_content_for_version_one()
     {
-        $data = $this->call('GET','/terms/2153/courses');
+        $data = $this->call('GET','/terms/' . $this->validTerm . '/courses');
         $content = json_decode($data->getContent(), true);
         $this->assertEquals($content['version'], '1.0');
         $this->assertEquals($content['api'], 'curriculum');
@@ -46,7 +51,7 @@ class TermControllerTest extends TestCase {
 
     public function testCoursesIndex_returns_json_content_for_version_one_point_one()
     {
-        $data = $this->call('GET','api/1.1/terms/2153/courses');
+        $data = $this->call('GET','api/1.1/terms/' . $this->validTerm . '/courses');
         $content = json_decode($data->getContent(), true);
         $this->assertEquals($content['version'], '1.1');
         $this->assertEquals($content['api'], 'curriculum');
@@ -55,5 +60,12 @@ class TermControllerTest extends TestCase {
         $this->assertEquals($content['collection'], 'courses');
         $this->assertEquals(count($content['courses']), 2878);
     }
+
+    public function testCoursesShow_returns_json_content_for_version_one()
+    {
+        $data = $this->call('GET', '/terms/'.$this->validTerm.'/courses/'.$this->validClassId);
+
+    }
+
 }
 
