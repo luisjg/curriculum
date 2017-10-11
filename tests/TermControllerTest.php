4f17b104ff044e267bc7d12e42ff7e87f47caf9b
joesdev <joesilvadev@gmail.com>
2017-10-11 11:51:06 -0700

    Added test function for TermController@classesIndex for version 1.1 route

--- tests/TermControllerTest.php
+++ tests/TermControllerTest.php
@@ -9,7 +9,8 @@ class TermControllerTest extends TestCase {
         parent::setUp();
     }
 
-    public function testClassesIndex_returns_json_content(){
+    public function testClassesIndex_returns_json_content_for_version_one()
+    {
         $data = $this->call('GET','/terms/2153/classes?instructor='.$this->validEmail);
         $content = json_decode($data->getContent(), true);
         $this->assertEquals($content['version'], '1.0');
@@ -19,6 +20,17 @@ class TermControllerTest extends TestCase {
         $this->assertEquals(count($content['classes']), 2);
     }
 
+    public function testClassesIndex_returns_json_content_for_version_one_point_one()
+    {
+        $data = $this->call('GET','api/1.1/terms/2153/classes?instructor='.$this->validEmail);
+        $content = json_decode($data->getContent(), true);
+        $this->assertEquals($content['version'], '1.1');
+        $this->assertEquals($content['api'], 'curriculum');
+        $this->assertEquals($content['status'], 200);
+        $this->assertEquals($content['success'], 'true');
+        $this->assertEquals($content['collection'], 'classes');
+        $this->assertEquals(count($content['classes']), 2);
+    }
 
 
 }
