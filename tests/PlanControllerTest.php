0a81f32f31dfb689bffc6f334f3183edb2f8fb0b
joesdev <joesilvadev@gmail.com>
2017-10-11 15:29:47 -0700

    Fixed route version number, added second test function

--- tests/PlanControllerTest.php
+++ tests/PlanControllerTest.php
@@ -8,8 +8,19 @@ class PlanControllerTest extends TestCase
         parent::setUp();
     }
 
-    public function testIndex_returns_json_content_for_all_versions()
+    public function testIndex_returns_json_content_for_version_one()
     {   $data = $this->call('GET','/plans');
+        $content = json_decode($data->getContent(), true);
+        $this->assertEquals($content['api'], 'curriculum');
+        $this->assertEquals($content['status'], '200');
+        $this->assertEquals($content['version'], '1.0');
+        $this->assertEquals($content['collection'], 'plans');
+        $this->assertEquals($content['limit'], '150');
+        $this->assertEquals(count($content['plans']), 148);
+    }
+
+    public function testIndex_returns_json_content_for_version_one_point_one()
+    {   $data = $this->call('GET','/api/1.1/plans');
         $content = json_decode($data->getContent(), true);
         $this->assertEquals($content['api'], 'curriculum');
         $this->assertEquals($content['status'], '200');
